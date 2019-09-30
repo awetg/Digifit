d13e32bdde264f9b3ee231c4411e8d8e1c6dae4d
bishwasshrestha <bishwas.shrestha@gmail.com>
2019-09-30 15:47:52 +0300

    Revert "Step count"

--- app/src/main/java/com/bwet/digifit/MainActivity.kt
+++ app/src/main/java/com/bwet/digifit/MainActivity.kt
@@ -1,37 +1,30 @@
 package com.bwet.digifit
 
-import android.app.Activity
-import android.app.StatusBarManager
 import android.os.Bundle
-import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.bwet.digifit.ui.main.SectionsPagerAdapter
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlinx.android.synthetic.main.bottom_sheet.*
 
-
-class MainActivity : AppCompatActivity(){
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        val bottomSheet: View = findViewById(R.id.bottom_sheet)
-        val behavior: BottomSheetBehavior<View> = BottomSheetBehavior.from(bottomSheet)
-
-
-
-
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
-
 }
--- app/src/main/java/com/bwet/digifit/Utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.bwet.digifit.Utils
-
-const val DEBUG_TAG = "DBG"
-
-const val PERMISSION_REQUEST_CODE_ACTIVITY_RECOGNITION = 1111
