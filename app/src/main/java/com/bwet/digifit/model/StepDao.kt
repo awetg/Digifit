4694c98845b6c287802c7e4ea777d6bb33ab2195
awetg <awetghebreslassie@gmail.com>
2019-10-02 00:27:32 +0300

    implemented data persistence for step count with room

---
+++ app/src/main/java/com/bwet/digifit/model/StepDao.kt
@@ -0,0 +1,27 @@
+package com.bwet.digifit.model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+
+@Dao
+interface StepDao {
+
+    @Insert( onConflict = REPLACE)
+    fun insert(step: Step)
+
+    // returns total number of steps insrted to database
+    @Query("SELECT COUNT(*) FROM step")
+    suspend fun getTotalStep(): Int
+
+    // returns step between two time stamps (timestamp in mills)
+    @Query("SELECT * FROM Step WHERE timeStampMills BETWEEN :startTimeMills AND :endTimeMills")
+    suspend fun getStepBetweenTime(startTimeMills: Long, endTimeMills: Long): List<Step>
+
+/*  Returns StepCount with number of steps grouped by interval(interval in seconds) of steps between two timestamp (timestamp in mills)
+    for interval formats refer https://www.sqlite.org/lang_datefunc.html
+    strftime('%Y-%m-%d %H:%M:%S', datetime(timeStampMills/1000, 'unixepoch', 'localtime')) */
+    @Query("SELECT COUNT(timeStampMills) as count, strftime(:intervalFormat, datetime(timeStampMills/1000, 'unixepoch', 'localtime')) as intervalFormat FROM step WHERE timeStampMills  BETWEEN :startTimeMills AND :endTimeMills GROUP BY round(timeStampMills / (1000 * :intervalSec))" )
+    suspend fun getStepBetweenTimeGroupedByInterval(startTimeMills: Long, endTimeMills: Long, intervalSec: Long, intervalFormat: String): List<StepCount>
+}
--- app/src/main/java/com/bwet/digifit/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/bwet/digifit/ui/main/SectionsPagerAdapter.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.bwet.digifit.PedometerFragment
+import com.bwet.digifit.view.PedometerFragment
 import com.bwet.digifit.R
 
 private val TAB_TITLES = arrayOf(
