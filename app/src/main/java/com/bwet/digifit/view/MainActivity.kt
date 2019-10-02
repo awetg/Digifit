d522589ef8ce2ffdbdd2b1e5a8cc6b99488c8df4
bishwasshrestha <bishwas.shrestha@gmail.com>
2019-10-02 23:47:10 +0300

    profile page with fundamental display

--- app/src/main/java/com/bwet/digifit/view/MainActivity.kt
+++ app/src/main/java/com/bwet/digifit/view/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.bwet.digifit.view
 
 import android.os.Bundle
-import android.view.View
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import com.bwet.digifit.R
 import com.bwet.digifit.ui.main.SectionsPagerAdapter
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class MainActivity : AppCompatActivity(){
@@ -22,13 +20,6 @@ class MainActivity : AppCompatActivity(){
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-
-        val bottomSheet: View = findViewById(R.id.bottom_sheet)
-        val behavior: BottomSheetBehavior<View> = BottomSheetBehavior.from(bottomSheet)
-
-
-
-
     }
 
 }
